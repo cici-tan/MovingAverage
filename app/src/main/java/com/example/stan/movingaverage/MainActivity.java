5f0caae2cc6fda81bf10c8249d11bfd2ae43813f
cici-tan <cici_t45@yahoo.com>
2017-02-14 20:00:03 -0800

    get mean value

--- app/src/main/java/com/example/stan/movingaverage/MainActivity.java
+++ app/src/main/java/com/example/stan/movingaverage/MainActivity.java
@@ -1,42 +1,35 @@
 package com.example.stan.movingaverage;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.PointF;
-import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.ViewGroup.LayoutParams;
-import android.view.Menu;
 import android.view.SurfaceView;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.HorizontalScrollView;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.affectiva.android.affdex.sdk.Frame;
 import com.affectiva.android.affdex.sdk.detector.CameraDetector;
 import com.affectiva.android.affdex.sdk.detector.Face;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.*;
+
+import static android.R.id.list;
 
 
 public class MainActivity extends AppCompatActivity implements CameraDetector.CameraEventListener, CameraDetector.ImageListener{
     SurfaceView cameraDetectorSurfaceView;
-//    HorizontalScrollView scrollWindow;
-    LinearLayout my_points;
-    CameraDetector cameraDetector;
-    TextView textViewCongrats;
-    TextView textViewMean;
-    int maxProcessingRate = 10;
-    float threshold = 75;
-    boolean isFull = false;
+    private LinearLayout my_points;
+    private CameraDetector cameraDetector;
+    private TextView textViewCongrats;
+    private int maxProcessingRate = 10;
+    private final float threshold = 60;
+    private final int numberOfPoint = 15;
+    private boolean isFull = false;
+    private List<Float> pointsList = new ArrayList<>();
 
 
     @Override
@@ -45,7 +38,7 @@ public class MainActivity extends AppCompatActivity implements CameraDetector.Ca
         setContentView(R.layout.activity_main);
 
         textViewCongrats = (TextView) findViewById(R.id.textViewCongrats);
-        textViewMean = (TextView) findViewById(R.id.textViewMean);
+//        textViewMean = (TextView) findViewById(R.id.textViewMean);
 
         cameraDetectorSurfaceView=(SurfaceView)findViewById(R.id.cameraDetectorSurfaceView);
         cameraDetector = new CameraDetector(this,CameraDetector.CameraType.CAMERA_FRONT,cameraDetectorSurfaceView);
@@ -56,8 +49,6 @@ public class MainActivity extends AppCompatActivity implements CameraDetector.Ca
 
         cameraDetector.setDetectAllEmotions(true);
         cameraDetector.start();
-
-        my_points = (LinearLayout)findViewById(R.id.my_points);
 
     }
 
@@ -76,26 +67,39 @@ public class MainActivity extends AppCompatActivity implements CameraDetector.Ca
 
     @Override
     public void onImageResults(List<Face> faces, Frame frame, float timeStamp) {
+
+        float mean;
+        float sum = 0;
+        String text = "You have reached high-level joy";
         // check if the frame was processed.
         if(faces == null)
             return;//frame was not processed.
-        if (faces.size() == 0)
-            textViewCongrats.setText("NO FACE");
+
         // check if there is any face recognized.
-//        if(faces.size() == 0)
-//            return;//no face
+        if (faces.size() == 0) {
+            return;
+        }
         Face face = faces.get(0);
-//        System.out.println("time"+timeStamp);
-//        System.out.println("gender: "+genderValue);
         float joy = face.emotions.getJoy();
-        if(joy > threshold)
-            textViewCongrats.setText("You reached high-level joy");
-//        float anger = face.emotions.getAnger();
-//        float surprise = face.emotions.getSurprise();
+
+        pointsList.add(joy);
+
+        if(pointsList.size() == numberOfPoint){
+            for(Object i : pointsList){
+                sum += (float)i;
+            }
+            mean = sum/numberOfPoint;
+            if(mean > threshold) {
+                textViewCongrats.setText(text);
+                System.out.println(text);
+            } else {
+                textViewCongrats.setText("");
+            }
+            pointsList.remove(0);
+        }
 
         System.out.println("Joy: " + joy);
-//        System.out.println("Anger: " + anger);
-//        System.out.println("Surprise: " + surprise);
+
     }
 
 }
